909b8927402732d219de6c5dc7578cb23a8ae624
Eduard Fedosevich <fedosewiched@gmail.com>
2021-09-20 02:51:20 +0300

    feat: add forms into contact block

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import styles from './Contacts.module.css'
+import {useForm, SubmitHandler} from "react-hook-form";
+
+type Inputs = {
+    example: string,
+    exampleRequired: string,
+};
 
 function Contacts() {
+
     return (
         <section className={styles.wrapper}>
             <div className={`${styles.content} container`}>
                 <div className={styles.content__wrapper}>
-                    <form action="text">
-                        <input type="text"/>
-                        <input type="email"/>
+                    <h3 className={styles.title}>Contact with me</h3>
+                    <form className={styles.form} >
+                        <input className={styles.input} type="text" placeholder={'Name'}/>
+                        <input className={styles.input}  type="email" placeholder={'Email'}/>
+                        <input className={styles.input_btn} type="submit"/>
                     </form>
                 </div>
             </div>
@@ -18,3 +27,6 @@ function Contacts() {
 
 
 export default Contacts;
+
+
+
